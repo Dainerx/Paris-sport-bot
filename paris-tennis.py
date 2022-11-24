66a42d4a9ddacc1fce5f8c8a1bc2a79f2d7fb1fb
BEN GHORBEL Oussama <d.oussamabenghorbel@gmail.com>
2022-11-24 14:54:31 +0100

    Support composed names - add requirements.txt

--- paris-tennis.py
+++ paris-tennis.py
@@ -55,6 +55,7 @@ def login(username, password):
         return driver
     except Exception as e:
         logging.error("Failure to login", e)
+        raise e
 
 
 def pick_tennis_court(driver, day, name):
@@ -73,12 +74,13 @@ def pick_tennis_court(driver, day, name):
         time.sleep(1)
     except Exception as e:
         logging.error("Failure to pick a tennis court", e)
+        raise e
 
 
 def book_and_pay(driver, hour, day, name):
     try:
         # Click on the arrow to make the booking rows available
-        driver.find_element_by_id('head{}{}'.format(name, hour)).click()
+        driver.find_element_by_id('head{}{}'.format(name.replace(" ", ""), hour)).click()
         time.sleep(2)
 
         # Click Reserver button
@@ -109,9 +111,10 @@ def book_and_pay(driver, hour, day, name):
             )
         else:
             logging.info(
-                "Held a reservation at {0}, in {1}, {2} days from today - you have 10 minutes to manually pay".format(hour,
-                                                                                                             name,
-                                                                                                             day))
+                "Held a reservation at {0}, in {1}, {2} days from today - you have 10 minutes to manually pay".format(
+                    hour,
+                    name,
+                    day))
             notification.notify(
                 title='Paris Sport Bot',
                 message="Held a reservation at {0}, in {1}, {2} days from today - you have 10 minutes to manually pay".format(
@@ -123,6 +126,7 @@ def book_and_pay(driver, hour, day, name):
             )
     except Exception as e:
         logging.error("Failure to book and pay", e)
+        raise e
 
 
 def check_arguments(args):
@@ -146,7 +150,8 @@ def check_arguments(args):
         logging.error("% s is not valid hour format" % args.Hour)
         return False
 
-    if not (str(args.Name).replace(" ", "") in PARIS_TENNIS_COURTS):
+    args.Name = str(args.Name).replace("-", " ")
+    if not (args.Name in PARIS_TENNIS_COURTS):
         logging.error("% s is not valid paris tennis court name" % args.Name)
         return False
 
@@ -165,12 +170,15 @@ if __name__ == '__main__':
     parser.add_argument("-ho", "--Hour", help="Set the hour to book")
     parser.add_argument(
         "-d", "--Day", help="Set in how many days from today to book")
-    parser.add_argument("-n", "--Name", help="Set tennis court name")
+    parser.add_argument("-n", "--Name", help="Set tennis court name - composed names should be separated by dash `-`")
     args = parser.parse_args()
     if not check_arguments(args):
         logging.error("Failure to validate arguments")
         exit(1)
 
+try:
     driver = login(PLAYER_USERNAME, PLAYER_PASSWORD)
     pick_tennis_court(driver, day=int(args.Day), name=args.Name)
     book_and_pay(driver, args.Hour, int(args.Day), args.Name)
+except Exception as e:
+    exit(1)
